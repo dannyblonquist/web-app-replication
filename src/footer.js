936fa1f5632a833b2f6c28591910c29ef16719d4
Danny Blonquist <danny@devpipeline.com>
2023-07-30 22:17:01 -0600

    done

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <div className="footer-container">
+        <div className="socials">SOCIAL MEDIA ICONS</div>
+        <div className="copyright">COPYRIGHT</div>
+      </div>
+    );
+  }
+}
